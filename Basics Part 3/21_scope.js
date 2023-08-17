24073c828b8cbf5a1a181203a1efa7ece5c26a72
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2023-08-18 01:31:39 +0530

    learn about nested scope

--- Basics Part 3/21_scope.js
+++ Basics Part 3/21_scope.js
@@ -1,6 +1,44 @@
- /*
- ? In this video learn about Gobal scope and local scope
+console.log("          ")
+/*
+ ? In this lecture learn about Gobal scope and local scope
 
  We take deep dive more var,const and let
  Why developer not prefer to use var
  */
+
+// Nested Scope
+
+function one(){
+     username="Ankit Ranjan"
+     let role;
+
+     function two(){
+          role="Full Stack develpoer"
+          console.log(username)
+     }
+     
+     console.log(role)
+     two()
+     console.log(role)
+}
+
+one()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("            ")
