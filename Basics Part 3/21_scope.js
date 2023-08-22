3071c3e12aa496665c5923fdc63cc7a8296a4c56
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2023-08-23 00:45:27 +0530

    lecture 22 completed

--- Basics Part 3/21_scope.js
+++ Basics Part 3/21_scope.js
@@ -1,4 +1,4 @@
-console.log("          ")
+console.log("          ");
 /*
  ? In this lecture learn about Gobal scope and local scope
 
@@ -8,37 +8,57 @@ console.log("          ")
 
 // Nested Scope
 
-function one(){
-     username="Ankit Ranjan"
-     let role;
+function one() {
+  username = "Ankit Ranjan";
+  let role;
 
-     function two(){
-          role="Full Stack develpoer"
-          console.log(username)
-     }
-     
-     console.log(role)
-     two()
-     console.log(role)
+  function two() {
+    role = "Full Stack develpoer";
+    console.log(username);
+  }
+
+  console.log(role);
+  two();
+  console.log(role);
 }
 
-one()
+// one()
+
+// Doing same thing in if else
+
+if (true) {
+  const username = "Ankit Ranjan";
+
+  if (username === "Ankit Ranjan") {
+    let learning = "HTML, CSS, Tailwind and indepth Javascript";
+    //     console.log(username);
+  }
+
+  //   console.log(learning);
+}
+
+/* 
+? Here we learn about something intresting 
+
+function output will about the return value or undefined
+it will print the console as well but with undefined if there is no return
+
+! And if we write anything after return then no code will execute
+
+? We learn about this error where in future we learn about Concept of Hoisting
+
+ReferenceError: Cannot access 'four' before initialization
+*/
+console.log(three(2));
+function three(num) {
+   return num + 2; 
+}
 
 
+console.log(four(4));
+const four = function (num) {
+  return num + 1;
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log("            ")
+console.log("            ");
