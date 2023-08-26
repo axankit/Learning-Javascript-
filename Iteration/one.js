3aff06ecb43bbeca7f5b3b031c4327fa888acbe6
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2023-08-26 21:12:25 +0530

    for loop done

---
+++ Iteration/one.js
@@ -0,0 +1,15 @@
+/*
+for loop in detail
+Nested for loop
+
+break => you are out of loop
+continue => skip the further execution of code for the one time
+ */
+
+for (let index = 0; index < 10; index++) {
+     if (index==6) {
+          console.log("Maza ane wala hai")
+          continue
+     }
+  console.log(index);
+}
